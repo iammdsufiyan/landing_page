397bf0eac1c96a4aab7743d61764041886ceea64
iammdsufiyan <uaremdsufian@gmail.com>
2026-09-01 13:35:25 +0530

    google anslytics

---
+++ assets/js/analytics.js
@@ -0,0 +1,15 @@
+(function () {
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
+
+  var gtagScript = document.createElement("script");
+  gtagScript.async = true;
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-1XEWNK2HGD";
+  document.head.appendChild(gtagScript);
+
+  window.gtag("js", new Date());
+  window.gtag("config", "G-1XEWNK2HGD");
+  window.gtag("config", "AW-11395309489");
+})();
